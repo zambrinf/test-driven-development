1e57234c299dbdafdb5f41deb057bea55b5efd8e
Felipe Zambrin <zambrinf@gmail.com>
2021-01-06 11:59:40 -0300

    First Commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertEquals(true, true);
+    }
+}
